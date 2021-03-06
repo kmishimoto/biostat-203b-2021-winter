5f44a0a17cec5007190544879f78d792c8a47c5b
Kelsey Ishimoto <kmishimoto@ucla.edu>
2021-03-06 00:53:35 +0000

    Created a helpers.R file to store the functions I previously created in the app.R file (makes the shiny app more readable) - fixed some plot and table labels

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -7,162 +7,13 @@ library(gridExtra)
 # Load data ----
 icu_cohort <- readRDS("icu_cohort.rds")
 
+# Source functions for tables and plots -----
+source("helpers.R")
+
 # Defining some indicator variables I will use later:
 deadind <- icu_cohort$death30days
 dead <- deadind == 1 & !is.na(deadind)
 alive <- deadind == 0 | is.na(deadind)
-
-# Defining some functions ------
-table1 <- function(input) {
-  # takes an input and outputs a html table of summary statistics
-  # outputs count, min, max, median, mean, sd, number missing
-  mean <- mean(input, na.rm = T)
-  sd <- sd(input, na.rm = T)
-  count <- length(input)
-  miss <- sum(is.na(input))
-  min <- min(input, na.rm = T)
-  max <- max(input, na.rm = T)
-  med <- median(input, na.rm = T)
-  
-  table2 <- matrix(c(as.integer(count), min, max, med, mean, sd, miss), nrow = 1)
-  colnames(table2) <- c("Count", "Min", "Max", "Median", "Mean", "StdDev",
-                        "Number Missing")
-  
-  xtable(table2, type = html)
-}
-
-table2 <- function(input1, input2) {
-  # takes two vector inputs and outputs a html table of summary statistics
-  mean <- round (c(mean(input1, na.rm = T), mean(input2, 
-                                                 na.rm = T)), 2)
-  sd <- round (c(sd(input1, na.rm = T), sd(input2, 
-                                           na.rm = T)), 2)
-  count <- c(length(input1), length(input2))
-  miss <- c(sum(is.na(input1)), sum(is.na(input2)))
-  min <- round(c(min(input1, na.rm = T), min(input2, na.rm = T)), 2)
-  max <- round(c(max(input1, na.rm = T), max(input2, na.rm = T)), 2)
-  med <- round(c(median(input1, na.rm = T), median(input2, na.rm = T)), 2)
-  
-  table1 <- matrix(c("Alive", "Dead", as.integer(count), min, max, med, mean, sd, miss), 
-                   nrow = 2, byrow = FALSE)
-  colnames(table1) <- c("Status at 30 days","Count", "Min", "Max", "Median", "Mean", "StdDev",
-                        "Number Missing")
-  xtable(table1, type = html)
-}
-
-hist1 <- function(input, varname, binnum) {
-  # outputs a histogram of the vector input,
-  # varname = string that is the variable name
-  # binnum = number of bins
-  ggplot() +
-    geom_histogram(mapping = aes(x = input), bins = binnum) +
-    xlab(varname) +
-    ylab("Count")
-}
-
-hist2 <- function(input1, input2, varname, binnum) {
-  plot0 <- ggplot() +
-    geom_histogram(mapping = aes(x = input1), 
-                   bins = binnum) +
-    xlab(varname) +
-    ylab("Count") + 
-    labs(title = "Patients who did not die within 30 days of admission")
-  
-  plot1 <- ggplot() +
-    geom_histogram(mapping = aes(x = input2), 
-                   bins = binnum) +
-    xlab(varname) +
-    ylab("Count") + 
-    labs(title = "Patients who died within 30 days of admission")
-  grid.arrange(plot0, plot1, ncol = 1)
-}
-
-barfreq1 <- function(input, varname) {
-  # barplot for a vector input
-  ggplot() +
-    geom_bar(mapping = aes(x = input)) +
-    theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-    xlab(varname) +
-    ylab("Count")
-}
-
-barfreq2 <- function(input1, input2, varname){
-  # two side-by-side frequency barplots of two vector inputs
-  # first input should be for patients alive at 30 day mark
-  plot0 <- ggplot() +
-    geom_bar(mapping = aes(x = input1)) +
-    theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-    xlab(varname) +
-    ylab("Count") + 
-    labs(title = "Patients who did not die within 30 days of admission")
-  
-  plot1 <- ggplot() +
-    geom_bar(mapping = aes(x = input2)) +
-    theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-    xlab(varname) +
-    ylab("Count") +
-    labs(title = "Patients who died within 30 days of admission")
-  
-  grid.arrange(plot0, plot1, ncol = 2)
-}
-
-barper1 <- function(input, varname){
-  # a bar plot showing percentages for a categorical vector input
-  # first input should be for patients alive at 30 day mark
-  tab <- round(prop.table(table(input, useNA = "ifany")), 2)*100
-  tab <- as.data.frame(tab, responseName = "Percentages")
-  ggplot() +
-    geom_col(mapping = aes(x = tab[, 1], y = tab$Percentages))+
-    theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-    xlab(varname) +
-    ylab("Percentages")
-}
-
-barper2 <- function(input1, input2, varname) {
-  tab0 <- round(prop.table(table(input1, useNA = "ifany")), 2)*100
-  tab0 <- as.data.frame(tab0, responseName = "Percentages")
-  plot0 <- ggplot() +
-    geom_col(mapping = aes(x = tab0[, 1], y = tab0$Percentages))+
-    theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-    xlab(varname) +
-    ylab("Percentages") +
-    labs(title = "Patients who did not die within 30 days of admission")
-  
-  tab1 <- round(prop.table(table(input2, useNA = "ifany")), 2)*100
-  tab1 <- as.data.frame(tab1, responseName = "Percentages")
-  plot1 <- ggplot() +
-    geom_col(mapping = aes(x = tab1[, 1], y = tab1$Percentages))+
-    theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1)) +
-    xlab(varname) +
-    ylab("Percentages") +
-    labs(title = "Patients who died within 30 days of admission")
-  
-  grid.arrange(plot0, plot1, ncol = 2)
-}
-
-
-box1 <- function(input, varname) {
-  ggplot() +
-    geom_boxplot(mapping = aes(y = input)) +
-    xlab(varname) +
-    ylab("Measurement")
-}
-
-box2 <- function(input1, input2, varname) {
-  # two boxplots of two different vectors
-  # first input should be for patients who were alive at 30 days after admission
-  plot0 <- ggplot() +
-    geom_boxplot(mapping = aes(y = input1)) +
-    xlab(varname) +
-    ylab("Measurement") + 
-    labs(title = "Patients who did not die within 30 days of admission")
-  plot1 <- ggplot() +
-    geom_boxplot(mapping = aes(y = input2)) +
-    xlab(varname) +
-    ylab("Measurement") + 
-    labs(title = "Patients who died within 30 days of admission")
-  grid.arrange(plot0, plot1, ncol = 2)
-}
 
 
 # User interface ----
@@ -368,7 +219,7 @@ server <- function(input, output) {
   })
   
   
-  # Demographic Table -------
+  # Demographic Tables -------
   
   output$demoTab <- renderTable({
     Factor <- switch(input$demovar,
@@ -388,7 +239,7 @@ server <- function(input, output) {
       
     } else if (input$prop == "Frequencies") {
       if (input$comp == "All Data") {
-        table(Factor, useNA = "ifany")
+        tabfreq1(Factor, input$demovar)
       } else if (input$comp == "Compare") {
         tab0 <- table(Factor[alive], useNA = "ifany")
         tab1 <- table(Factor[dead], useNA = "ifany")
@@ -400,9 +251,10 @@ server <- function(input, output) {
     } else if (input$prop == "Percentages") {
       
       if (input$comp == "All Data") {
-        tab <- round(prop.table(table(Factor, useNA = "ifany")), 2)*100
-        tab <- as.data.frame(tab, responseName = "Percentages")
-        xtable(tab, type = html)
+        tabper1(Factor, input$demovar)
+        # tab <- round(prop.table(table(Factor, useNA = "ifany")), 2)*100
+        # tab <- as.data.frame(tab, responseName = "Percentages")
+        # xtable(tab, type = html)
       } else if (input$comp == "Compare") {
         tab0 <- round(prop.table(table(Factor[alive], useNA = "ifany")), 
                       2) * 100
@@ -467,7 +319,7 @@ server <- function(input, output) {
       
     } else if (input$prop2 == "Frequencies") {
       if (input$comph == "All Data") {
-        table(Factor, useNA = "ifany")
+        tabfreq1(Factor, input$hosp)
       } else if (input$comph == "Compare") {
         tab0 <- table(Factor[alive], useNA = "ifany")
         tab1 <- table(Factor[dead], useNA = "ifany")
@@ -479,16 +331,14 @@ server <- function(input, output) {
       
     } else if (input$prop2 == "Percentages") {
       if (input$comph == "All Data") {
-        tab <- round(prop.table(table(Factor, useNA = "ifany")), 2)*100
-        tab <- as.data.frame(tab, responseName = "Percentages")
-        xtable(tab, type = html)
+        tabper1(Factor, input$hosp)
       } else if (input$comph == "Compare") {
-        tab0 <- round(prop.table(table(Factor[alive], useNA = "ifany")), 
+        tab0 <- round(prop.table(table(Factor[alive], useNA = "ifany")),
                       2) * 100
-        tab1 <- round(prop.table(table(Factor[dead], useNA = "ifany")), 
+        tab1 <- round(prop.table(table(Factor[dead], useNA = "ifany")),
                       2) * 100
         table <- left_join(as.data.frame(tab0), as.data.frame(tab1), by = "Var1")
-        names(table) <- c(input$hosp, "Percent (Alive at 30 Days)", 
+        names(table) <- c(input$hosp, "Percent (Alive at 30 Days)",
                           "Percent (Dead at 30 Days)")
         xtable(table, type = html)
       }
